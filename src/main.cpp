65d16b4a79080c19de65ae4d413285aff6a62865
Caden Jurkovski <jurko036@umn.edu>
2024-12-25 19:30:40 -0600

    finished initial code

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,80 @@
 #include <Arduino.h>
 #include "Arduino_LED_Matrix.h"
 
+#define BUTTON_PIN 5
+#define MOTOR_FI_PIN 9
+#define MOTOR_BI_PIN 10
+
 ArduinoLEDMatrix matrix;
 
-void setup() {
-    matrix.begin();
-}
-
-
-uint8_t frame[8][12] = {
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+uint8_t forwardFrame[8][12] = {
+  { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+  { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 }
 };
 
+uint8_t backwardFrame[8][12] = {
+  { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 },
+  { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0 },
+  { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+uint8_t standbyFrame[8][12] = {
+  { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+  { 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+  { 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+  { 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 }
+};
+
+int current_setting = 0;
+
+void setup() {
+    pinMode(MOTOR_FI_PIN, OUTPUT);
+    pinMode(MOTOR_BI_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+    Serial.begin(9600);
+    matrix.begin();
+    matrix.renderBitmap(standbyFrame, 8, 12);
+}
+
 void loop() {
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 12; j++) {
-            frame[i][j] = 1;
-            frame[j][i] = 1;
-            matrix.renderBitmap(frame, 8, 12);
-            delay(50);
-            frame[i][j] = 0;
-            frame[j][i] = 0;
-            matrix.renderBitmap(frame, 8, 12);
-        }
+    int buttonState = digitalRead(BUTTON_PIN);
+
+    if (buttonState == LOW) {
+        current_setting = (current_setting + 1) % 3;
+        delay(200);
+    }
+
+    switch (current_setting) {
+        case 0:
+            digitalWrite(MOTOR_FI_PIN, LOW);
+            digitalWrite(MOTOR_BI_PIN, LOW);
+            matrix.renderBitmap(standbyFrame, 8, 12);
+            break;
+        case 1:
+            digitalWrite(MOTOR_FI_PIN, HIGH);
+            digitalWrite(MOTOR_BI_PIN, LOW);
+            matrix.renderBitmap(forwardFrame, 8, 12);
+            break;
+        case 2:
+            digitalWrite(MOTOR_FI_PIN, LOW);
+            digitalWrite(MOTOR_BI_PIN, HIGH);
+            matrix.renderBitmap(backwardFrame, 8, 12);
+            break;
     }
 }
